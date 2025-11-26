0bc839c21b6c685a6ef46c24fc703b980e5b41f8
raviteja <ravitejaraju1999@gmail.com>
2025-11-26 18:15:15 +0530

    variables concept

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,8 +2,8 @@
 
 #No space before or after equal
 
-person1 = john
-person2 = raheem
+person1=john
+person2=raheem
 
 echo "$person1:: hi suresh, howe are you?"
 echo "$person2:: Hello Ramesh,I am fine.How are you?"
