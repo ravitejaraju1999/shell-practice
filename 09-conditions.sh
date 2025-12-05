f341f158272857ba41575832cb6b6c0a4ec1477b
raviteja <ravitejaraju1999@gmail.com>
2025-12-05 15:59:19 +0530

    maths operations

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,6 +4,10 @@ number=$1
 
 if [ $number -lt 10 ]; then
     echo "given number $number is less than 10"
+
+elif [$number -eq 10]; then
+
+    echo " given $number is equal to 10"
 else
     echo "given number is $number is greater than or equal to 10"
 
