da67cf1b01f156c7bb16ecd1bb61087c489953a2
raviteja <ravitejaraju1999@gmail.com>
2025-12-06 10:50:49 +0530

    installationcolors

--- 14-colorsinstall.sh
+++ 14-colorsinstall.sh
@@ -7,10 +7,10 @@ N="\e[0m"
 
 validate(){ # functions recieve inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo "error :: Installing $2 is failure"
+        echo -e"error :: Installing $2 is $R failure $N"
         exit 1
     else
-        echo "installing $2 is sucess"
+        echo -e"installing $2 ...$G is sucess $N"
     fi
 }
 
@@ -19,7 +19,7 @@ if [ $? -ne 0 ];then
     dnf install mysql -y
     validate $? "mysql"
 else
-    echo "mysql is already exist"
+    echo "$Y mysql is already exist $N"
 fi
 
 dnf list installed mysql
@@ -27,7 +27,7 @@ if [ $? -ne 0 ];then
     dnf install nginx -y
     validate $? "nginx"
 else
-    echo "nginx is already exist"
+    echo "$Y nginx is already exist $N"
 fi
 
 dnf list installed mysql
@@ -35,7 +35,7 @@ if [ $? -ne 0 ];then
     dnf install python3 -y
     validate $? "python3"
 else
-    echo "python3 is already exist"
+    echo "$Y python3 is already exist $N"
 fi
 
 
