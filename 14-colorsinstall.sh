925977751559edb04151b62493e02e1ba4534352
raviteja <ravitejaraju1999@gmail.com>
2025-12-06 10:51:44 +0530

    installationcolors

--- 14-colorsinstall.sh
+++ 14-colorsinstall.sh
@@ -19,7 +19,7 @@ if [ $? -ne 0 ];then
     dnf install mysql -y
     validate $? "mysql"
 else
-    echo "$Y mysql is already exist $N"
+    echo -e"$Y mysql is already exist $N"
 fi
 
 dnf list installed mysql
@@ -27,7 +27,7 @@ if [ $? -ne 0 ];then
     dnf install nginx -y
     validate $? "nginx"
 else
-    echo "$Y nginx is already exist $N"
+    echo -e"$Y nginx is already exist $N"
 fi
 
 dnf list installed mysql
@@ -35,7 +35,7 @@ if [ $? -ne 0 ];then
     dnf install python3 -y
     validate $? "python3"
 else
-    echo "$Y python3 is already exist $N"
+    echo -e"$Y python3 is already exist $N"
 fi
 
 
